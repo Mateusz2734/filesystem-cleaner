2db366cbb1092b8a7a6ba3a6c367cff6e25bc4c9
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-11-29 14:18:42 +0100

    Implement FileDuplicateFinder and FileVersionsFinder with tests

--- src/main/java/pl/edu/agh/to2/cleaner/command/FileDuplicateFinder.java
+++ src/main/java/pl/edu/agh/to2/cleaner/command/FileDuplicateFinder.java
@@ -1,4 +1,14 @@
 package pl.edu.agh.to2.cleaner.command;
 
-public class FileDuplicateFinder {
+import pl.edu.agh.to2.cleaner.model.FileInfo;
+import pl.edu.agh.to2.cleaner.model.comparator.CompositeFileComparator;
+import pl.edu.agh.to2.cleaner.model.comparator.NameEqualityComparator;
+import pl.edu.agh.to2.cleaner.model.comparator.SizeEqualityComparator;
+
+import java.util.List;
+
+public class FileDuplicateFinder extends FileGroupFinder {
+    public FileDuplicateFinder(List<FileInfo> files) {
+        super(files, new CompositeFileComparator(List.of(new NameEqualityComparator(), new SizeEqualityComparator()), true));
+    }
 }
