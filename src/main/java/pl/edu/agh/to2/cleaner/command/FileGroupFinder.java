8443cc98034cc4bfcc2fc55c3a55fc35730a8134
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-11-29 14:18:42 +0100

    Implement FileDuplicateFinder and FileVersionsFinder with tests

---
+++ src/main/java/pl/edu/agh/to2/cleaner/command/FileGroupFinder.java
@@ -0,0 +1,34 @@
+package pl.edu.agh.to2.cleaner.command;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import pl.edu.agh.to2.cleaner.model.FileInfo;
+import pl.edu.agh.to2.cleaner.model.comparator.FileComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class FileGroupFinder {
+    private final List<FileInfo> files;
+    private final FileComparator comparator;
+
+    public FileGroupFinder(List<FileInfo> files, FileComparator comparator) {
+        this.files = files;
+        this.comparator = comparator;
+    }
+
+    public List<ImmutablePair<FileInfo, FileInfo>> find() {
+        var connections = new ArrayList<ImmutablePair<FileInfo, FileInfo>>();
+
+        for (int i = 0; i < files.size(); i++) {
+            for (int j = 0; j < files.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (comparator.compare(files.get(i), files.get(j))) {
+                    connections.add(new ImmutablePair<>(files.get(i), files.get(j)));
+                }
+            }
+        }
+        return connections;
+    }
+}
