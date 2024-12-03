fd602626d64fbb8c744cfc4257b0937144d81588
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-12-03 12:18:29 +0100

    Add example of full file walk with grouping of files

---
+++ src/test/java/pl/edu/agh/to2/cleaner/examples/Grouping.java
@@ -0,0 +1,56 @@
+package pl.edu.agh.to2.cleaner.examples;
+
+import pl.edu.agh.to2.cleaner.command.FileDuplicateFinder;
+import pl.edu.agh.to2.cleaner.command.FileVersionsFinder;
+import pl.edu.agh.to2.cleaner.command.UnionFind;
+import pl.edu.agh.to2.cleaner.model.FileInfo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class Grouping {
+    public static void main(String[] args) throws IOException {
+        var dir = "example_dir";
+        var files = Files.walk(Path.of(dir))
+                .filter(Files::isRegularFile)
+                .map(path -> {
+                    try {
+                        return new FileInfo(path.toFile());
+                    } catch (Exception e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .toList();
+
+        {
+            var connections = new FileVersionsFinder(files).find();
+            var groups = new UnionFind().connectedComponentsFromEdges(connections);
+
+            System.out.println("Versions (similar name):");
+            for (var group : groups) {
+                System.out.println("Group:");
+                for (var file : group) {
+                    System.out.println("\t" +file.getPath());
+                }
+            }
+        }
+
+        System.out.println("\n");
+
+        {
+            var connections = new FileDuplicateFinder(files).find();
+            var groups = new UnionFind().connectedComponentsFromEdges(connections);
+
+            System.out.println("Duplicates (same size and name):");
+            for (var group : groups) {
+                System.out.println("Group:");
+                for (var file : group) {
+                    System.out.println("\t" + file.getPath());
+                }
+            }
+        }
+    }
+}
