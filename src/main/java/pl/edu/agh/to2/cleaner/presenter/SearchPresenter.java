ecf24b27ebe0fa8d9e1ec00e324864dc3f98e18d
Marcin Walendzik <mracin.walendzik.dysk@gmail.com>
2024-12-17 13:31:07 +0100

    Searching for files in gui almost done

--- src/main/java/pl/edu/agh/to2/cleaner/presenter/SearchPresenter.java
+++ src/main/java/pl/edu/agh/to2/cleaner/presenter/SearchPresenter.java
@@ -3,20 +3,45 @@ package pl.edu.agh.to2.cleaner.presenter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import pl.edu.agh.to2.cleaner.command.FileDuplicateFinder;
+import pl.edu.agh.to2.cleaner.command.FileVersionsFinder;
+import pl.edu.agh.to2.cleaner.command.UnionFind;
+import pl.edu.agh.to2.cleaner.model.FileInfo;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 
 public class SearchPresenter {
 
     private Stage stage;
+
+    @FXML
+    private Label errorLabel;
+    
     @FXML
     private TextField pathTextField;
+
+    @FXML
+    private CheckBox versionCheckbox;
+
+    @FXML
+    private CheckBox duplicateCheckbox;
+
     @FXML
     private Button fileChooser;
+
+    @FXML
+    private Button searchFiles;
 
     public SearchPresenter() {
 
@@ -28,11 +53,74 @@ public class SearchPresenter {
 
     @FXML
     public void openFileChooser(ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Znajdź plik");
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Find destination");
 
-        File chosenFile = fileChooser.showOpenDialog(stage);
-        System.out.println(chosenFile.getPath());
+//        File startingDirectory = new File(System.getProperty("user.home"));
+//        if (startingDirectory.exists()) {
+//            System.out.println("LOLO");
+//            fileChooser.setInitialDirectory(startingDirectory);
+//        }
+
+        File chosenFile = directoryChooser.showDialog(stage);
+
+        if (chosenFile != null) {
+            pathTextField.setText(chosenFile.getAbsolutePath());
+            System.out.println(chosenFile.getAbsolutePath());
+        }
     }
 
+    @FXML
+    public void searchForFiles(ActionEvent event) throws IOException {
+        var dir = pathTextField.getText();
+
+        if (dir.isEmpty()) {
+            errorLabel.setText("Enter the proper destination");
+        }
+        else if (!versionCheckbox.isSelected() && !duplicateCheckbox.isSelected()) {
+            errorLabel.setText("Choose variant of search");
+        }
+        else {
+            errorLabel.setText("");
+            var files = Files.walk(Path.of(dir))
+                    .filter(Files::isRegularFile)
+                    .map(path -> {
+                        try {
+                            return new FileInfo(path.toFile());
+                        } catch (Exception e) {
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .toList();
+
+            {
+                var connections = new FileVersionsFinder(files).find();
+                var groups = new UnionFind().connectedComponentsFromEdges(connections);
+
+                System.out.println("Versions (similar name):");
+                for (var group : groups) {
+                    System.out.println("Group:");
+                    for (var file : group) {
+                        System.out.println("\t" +file.getPath());
+                    }
+                }
+            }
+
+            System.out.println("\n");
+
+            {
+                var connections = new FileDuplicateFinder(files).find();
+                var groups = new UnionFind().connectedComponentsFromEdges(connections);
+
+                System.out.println("Duplicates (same size and name):");
+                for (var group : groups) {
+                    System.out.println("Group:");
+                    for (var file : group) {
+                        System.out.println("\t" + file.getPath());
+                    }
+                }
+            }
+        }
+    }
 }
