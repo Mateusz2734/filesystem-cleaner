dc057ec507ad7171157928645886dda842f8524d
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-01-21 15:33:23 +0100

    Update configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig(({command}) => ({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  logLevel: command === 'build' ? 'silent': "info",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  logLevel: command === "build" ? "silent" : "info",
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // Spring Boot server
+      "/api": {
+        target: "http://localhost:8080",
         changeOrigin: true,
       },
     },
